d57f77b9b76df51769f1a11ee2383a5d01612026
dimitrij <dimitrij@aleshkov.com>
2020-10-21 00:29:04 +0200

    JSON updated

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const app = document.getElementById('root');
 
 const container = document.createElement('div');
+
 container.setAttribute('class', 'container');
 
 app.appendChild(container);
@@ -12,9 +13,10 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
+    
     data.forEach(persons => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+      const person = document.createElement('div');
+      person.setAttribute('class', 'person');
 
       const h1 = document.createElement('h1');
       h1.textContent = persons.title;
@@ -23,9 +25,9 @@ request.onload = function () {
       persons.description = persons.description.substring(0, 300);
       p.textContent = `${persons.description}...`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
+      container.appendChild(person);
+      person.appendChild(h1);
+      person.appendChild(p);
     });
   } 
   // If there no such file or bad request. 
